9fa7fd3a801256b5de334dbf216e1f1204e7a2b9
Bibek Bhandari <bibek09@outlook.com>
2020-03-06 12:02:52 -0600

    Javascript added for animation

--- main/views.py
+++ main/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
-
+import json
+import requests
 # Create your views here.
 def home(request):
     if request.user.is_authenticated:
         # context for the website
+        # api call
+        time = getTime()['time']
         context = {
-            
+            "time": time
         }
         return render(request, 'main/index.html', context)
     else:
         return redirect("accounts:login")
 
 # here is the main logic for the program
+# api call for time 
+def getTime():
+    requestURL = "http://ec2-3-14-152-181.us-east-2.compute.amazonaws.com/api/data/gettime"
+
+    # get data from the api
+    response = requests.get(requestURL)
+
+    return response.json()
+
